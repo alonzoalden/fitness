28d7417b098a80987bdc30a48530c25b8355426d
Alonzo Alden <alonzoalden@gmail.com>
2017-03-16 14:35:14 -0700

    set up sorting algorithm pseudocode

--- src/App.js
+++ src/App.js
@@ -12,28 +12,63 @@ class App extends Component {
   }
 
   authorize() {
-    var yearAgoEpoch = moment().utc().subtract(365, 'days').unix();
+    var yearAgoISO = moment().utc().subtract(365, 'days');
+    var yearAgoEpoch = yearAgoISO.unix();
+
 
     axios.get('https://www.strava.com/api/v3/athlete/activities', {
       params: {
         access_token: "e6f5be775faf79655e23c8d71dd059fbdc7bdb96",
         per_page: 10,
-        before: yearAgoEpoch
+        after: yearAgoEpoch
       }
     })
       .then((response) => {
         return !response ? 'Error retrieving data from Strava API' : response;
       })
       .then((response) => {
-        console.log(response.data)
+        console.log(response)
+        var b = moment(yearAgoISO)
+        console.log('b', b.from(response.data[2].start_date))
+
+        console.log(moment(response.data[1].start_date))
+        console.log(response.data[2].start_date)
+        var rides = [];
+
+
+        for (var i = 0; i < response.data.length; i++) {
+          rides.push(response.data[i])
+          //set up holder to remember last date.
+          //compare current date to last date
+            //if it's 1 day,
+              //rides.push(ride)
+            //if its 2+ days
+              //daysBetween = days - 1
+              //set up while (daysBetween !== 0)
+                //daysBetween---
+                //rides.push(0)
+              //rides.push(ride)
+
+        }
+        console.log(rides)
+        //start with a how long ago from one year
+
+
+
+
         //write algorithm that does a check for the last ride from the current ride using momentjs. for each amount of days in between each ride, push that many 0's into the array. these will represent off days where fitness will drop.
 
         //if total fitness <20, multiply by 3
         // < 40 multiply by 2
         // < 60 multiply by 1
-
         //i.e. tapers off over time.
+
+
       })
+  }
+
+  sortFitness(data) {
+
   }
 
   componentDidMount() {
