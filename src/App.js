fd961f65212c969b21edf3e46c5646b26b19e79e
Alonzo Alden <alonzoalden@gmail.com>
2017-03-16 21:55:19 -0700

    created array for dates for d3 ticks and tweaked my algorithm to accomodate for missed days

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
+      dates: [],
     }
   }
 
@@ -45,9 +46,10 @@ class App extends Component {
     console.log('XXX', moment('2016-12-28T12:17:02Z').from('2017-01-24T11:48:00Z'))
     for (var i = 0; i < response.data.length; i++) {
       var currentDate = moment(response.data[i].start_date_local);
-      var dateDiff = !lastDate ? currentDate.from(timeAgo) : currentDate.from(lastDate);
+      var dateDiff = !lastDate ? currentDate.from(timeAgoISO) : currentDate.from(lastDate);
       var daysBetween = 0;
       var timeInWords = dateDiff.split(" ");
+
       if (timeInWords[2] === 'days') {
         daysBetween = Number(timeInWords[1] - 1);
         while (daysBetween !== 0) {
@@ -75,35 +77,46 @@ class App extends Component {
       }
 
       if (timeInWords[2] === 'hours') {
-        if (Number(timeInWords[1]) < 14) {
+        if (Number(timeInWords[1]) < 14 && i !== 0) {
+          console.log('ss')
           continue;
         }
       }
       rides.push(response.data[i].start_date_local);
       lastDate = currentDate;
     }
+    if (rides.length < this.state.dates.length) {
+      while (rides.length < this.state.dates.length) {
+        rides.unshift(0);
+      }
+    }
     return rides;
   }
 
   createDatesArray() {
-    var timeAgoISO = moment().utc().subtract(180, 'days').format();
+    var timeAgoISO = moment().utc().subtract(179, 'days').format();
+
     var date1 = new Date();
     var date2 = new Date(timeAgoISO);
     var day;
-    var datesBetween = [];
+    var datesBetween = [date1.toString().split('').slice(4, 15).join('')];
 
-    while(date2 <= date1) {
+    while(date2 < date1) {
         day = date1.getDate()
         date1 = new Date(date1.setDate(--day));
         var formattedDate = date1.toString().split('').slice(4, 15).join('');
         datesBetween.push(formattedDate);
     }
-    console.log(datesBetween)
+
+    this.setState({
+      dates: datesBetween.reverse(),
+    })
   }
 
   componentDidMount() {
     this.authorize();
     this.createDatesArray();
+    console.log(this.state)
   }
 
 
