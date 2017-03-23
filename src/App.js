30bb09c83eaf3e57ccf30645eb65df89d6f04976
Alonzo Alden <alonzoalden@gmail.com>
2017-03-22 20:03:02 -0700

    more graph, more sorting algo (3rd renovation)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import FitnessChart from './components/FitnessChart.js';
+// import FitnessChart from './components/FitnessChart.js';
 import axios from 'axios';
 import moment from 'moment';
 // import LineChart from './components/LineChart.js'
@@ -9,6 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      formattedData: [],
       data: [],
       dates: [],
     }
@@ -29,12 +30,44 @@ class App extends Component {
       return !response ? 'Error retrieving data from Strava API' : response;
     })
     .then((response) => {
-      var fitnessData = this.sortFitnessNew(response);
-      // this.setState({
-      //   data: fitnessData
-      // })
-      console.log(fitnessData)
+      this.createDatesArray();
+      console.log(this.state)
+      return response
     })
+    // .then((response) => {
+    //   let fitnessData = this.sortFitnessNew(response);
+    //   this.setState({
+    //     data: response.data,
+    //   })
+    //   console.log(new Date(this.state.data[1].start_date_local.slice(0,10)).toString().slice(4,15))
+    //   console.log(this.state)
+    // })
+  }
+
+  sortFitnessNew(response) {
+
+    for (var i = 0; i < response.data.length; i++) {
+      let startI = 0;
+      for (var j = startI-1; j < this.state.formattedData.length; j++) {
+        let currentRide = response.data[i];
+        let currentRideDate = response.data[i].start_date_local.slice(0,10).toString().slice(4,15);
+        let currentDate = this.state.formattedData[j].formattedDate;
+        let startI = 0;
+        if (currentRideDate === currentDate) {
+          currentRide.formattedDate = currentRideDate;
+          this.state.formattedData[j] = currentRide;
+
+        }
+      }
+      startI++
+    }
+    //create for loop, for each response.data
+      //create another for loop for dates
+      //iterate thru the whole dates array
+        //do a match of start_date_local and dates.formattedDate
+          //if true
+            // extend this formattedDate on to (data[i].formattedDate = dates[i].formattedDate)
+            //iMemory = i - 1
   }
 
   //new idea for sortFitness
@@ -46,53 +79,53 @@ class App extends Component {
 
   //should return an array, all objects should have the corresponding date
 
-  sortFitnessNew(response) {
-    let startDate = moment().utc().subtract(180, 'days')
-    let rideData = [];
-    let lastDate = null;
+  // sortFitnessNew(response) {
+  //   let startDate = moment().utc().subtract(180, 'days')
+  //   let rideData = [];
+  //   let lastDate = null;
 
-    const timeBetween = (startDate, endDate) => {
-      let startDateStr = new Date(startDate).toString().split('').slice(0, 15).join('');
-      let endDateStr = new Date(endDate).toString().split('').slice(0, 15).join('');
-      let date1 = new Date(startDateStr);
-      let date2 = new Date(endDateStr);
-        console.log(date1)
-        console.log(date2)
-      let dayCount = Math.ceil(((date2 - date1) / 3600000) / 24);
-      let day;
-        console.log(dayCount)
-      while(dayCount > 1) {
-          dayCount--
+  //   const timeBetween = (startDate, endDate) => {
+  //     let startDateStr = new Date(startDate).toString().split('').slice(0, 15).join('');
+  //     let endDateStr = new Date(endDate).toString().split('').slice(0, 15).join('');
+  //     let date1 = new Date(startDateStr);
+  //     let date2 = new Date(endDateStr);
+  //       console.log(date1)
+  //       console.log(date2)
+  //     let dayCount = Math.ceil(((date2 - date1) / 3600000) / 24);
+  //     let day;
+  //       console.log(dayCount)
+  //     while(dayCount > 1) {
+  //         dayCount--
 
-          day = date1.getDate();
-          date1 = new Date(date1.setDate(day+=1));
-          console.log("this should be +1 day after the startdate/lastdate", new Date(date1))
-          var formattedDate = date1.toString().split('').slice(0, 15).join('');
+  //         day = date1.getDate();
+  //         date1 = new Date(date1.setDate(day+=1));
+  //         console.log("this should be +1 day after the startdate/lastdate", new Date(date1))
+  //         var formattedDate = date1.toString().split('').slice(0, 15).join('');
 
-          rideData.push({ start_date_new: formattedDate });
-      }
+  //         rideData.push({ start_date_new: formattedDate });
+  //     }
 
-      lastDate = date2.toString().split('').slice(0, 15).join('');
+  //     lastDate = date2.toString().split('').slice(0, 15).join('');
 
-    }
+  //   }
 
-      for (let i = 0; i < response.data.length; i++) {
-        let currentTimeData = response.data[i].start_date_local.slice(0,10);
+  //     for (let i = 0; i < response.data.length; i++) {
+  //       let currentTimeData = response.data[i].start_date_local.slice(0,10);
 
-        !lastDate ? timeBetween(startDate, currentTimeData) : timeBetween(lastDate, currentTimeData);
-        // response.data[i].start_date_new = lastDate;
-        rideData.push(response.data[i]);
-      }
-
-
-
-      //take lastDate and currentDate
-      //iterate with a while loop until it get's there
-        //for all iterations, add to the array, an object of the current iteration's date with property called start_time_local
+  //       !lastDate ? timeBetween(startDate, currentTimeData) : timeBetween(lastDate, currentTimeData);
+  //       // response.data[i].start_date_new = lastDate;
+  //       rideData.push(response.data[i]);
+  //     }
 
 
-    return rideData;
-  }
+
+  //     //take lastDate and currentDate
+  //     //iterate with a while loop until it get's there
+  //       //for all iterations, add to the array, an object of the current iteration's date with property called start_time_local
+
+
+  //   return rideData;
+  // }
 
 
   sortFitness(response) {
@@ -163,27 +196,24 @@ class App extends Component {
         day = date1.getDate()
         date1 = new Date(date1.setDate(--day));
         var formattedDate = date1.toString().split('').slice(4, 15).join('');
-        datesBetween.push(formattedDate);
+        datesBetween.push({ formattedDate : formattedDate });
     }
 
+
     this.setState({
-      dates: datesBetween.reverse(),
+      formattedData: datesBetween.reverse(),
     })
   }
 
   componentDidMount() {
     this.authorize();
-    this.createDatesArray();
+
 
   }
 
   render() {
 
-    if (this.state.data.length === 0) {
-      return (
-        <p>loading</p>
-      );
-    }
+
     return (
       <div className="App">
         <div className="App-header">
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactFauxDOM from 'react-faux-dom';
 import * as d3 from 'd3';
-
+import moment from 'moment';
 
 class LineChart extends React.Component {
   constructor(props) {
@@ -12,75 +12,105 @@ class LineChart extends React.Component {
     }
   }
 
-
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      data: nextProps.data,
+      dates: nextProps.dates
+    })
+  }
 
   render() {
 
+    if (this.state.data.length === 0) {
+      return (
+        <p>loading</p>
+      );
+    }
     // set units, margin, sizes
+    var margin = { top: 20, right: 20, bottom: 70, left: 50 };
+    var width = 700 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+    var padding = 100
+    //parse date / time
+    var parseTime = d3.timeParse("%Y-%m-%d");
 
-    var margin = { top: 10, right: 0, bottom: 10, left: 0 };
-    var width = 690 - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
-
-    var format = (d) => formatNumber(d);
-    var formatNumber = d3.format(",.0f"); // zero decimal places
-
-    var graph = {
-      data: [],
-      dates: [],
-    };
+    //set ranges
+    var x = d3.scaleTime().rangeRound([0, width]);
+    var y = d3.scaleLinear().rangeRound([height, 0]);
 
 
     // initialize and append the svg canvas to faux-DOM
     var node = ReactFauxDOM.createElement('div');
 
     var svg = d3.select(node).append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
 
-    // add data
-    var link = svg.append("g").selectAll(".link")
-      .data(graph.links)
-      .enter().append("path")
-      .attr("class", "link")
-      .on('click', this.props.openModal) // register eventListener
-      .attr("d", path)
-      .style("stroke-width", (d) => Math.max(1, d.dy))
-
-    // add data titles
-    link.append("title")
-      .text((d) => d.source.name + " → " + d.target.name + "\n Weight: " + format(d.value));
+      // format the data
+      this.state.data.forEach((d) => {
 
 
-    // add nodes
-    var node = svg.append("g").selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .on('click', this.props.openModal) // register eventListener
-      .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
+          d.kilojoules = !d.kilojoules ? 0 : +d.kilojoules;
 
-    // add nodes rect
-    node.append("rect")
-      .attr("height", (d) => d.dy)
-      .attr("width", sankey.nodeWidth())
-      .append("title")
-      .text((d) => d.name + "\n" + format(d.value));
+          console.log(d.kilojoules)
+      });
 
-    // add nodes text
-    node.append("text")
-      .attr("x", -6)
-      .attr("y", (d) => d.dy / 2)
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .text((d) => d.name)
-      .filter((d) => d.x < width / 2)
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
+      this.state.dates.forEach((d) => {
+          d = parseTime(new Date(d).toISOString().slice(0,10));
+
+
+      });
+      // Scale the range of the data
+      x.domain([new Date(this.state.dates[0]), new Date(this.state.dates[this.state.dates.length-1])]);
+
+      y.domain([0, d3.max(this.state.data, function(d) { return d.kilojoules; })]);
+
+    // //define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d); })
+        .y(function(d) { return y(d.kilojoules); });
+
+      // Add the valueline path.
+      svg.append("path")
+          .datum(this.state.data)
+          .attr("fill", "none")
+          .attr("stroke", "steelblue")
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .attr("stroke-width", 1.5)
+          .attr("d", valueline);
+
+
+      // Add the X Axis
+      svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+        .tickFormat(d3.timeFormat("%b-%d-%Y")))
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)");
+
+
+      // Add the Y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("fill", "#000")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "0.71em")
+          .attr("text-anchor", "end")
+          .text("kilojoules");
 
     return node.toReact();
 
+  }
 };
-}
+
+export default LineChart;
